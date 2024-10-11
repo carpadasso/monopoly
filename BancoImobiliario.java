99d7991d96c84737a70b121c82f6a3074b9f6a98
Leonardo Carpwiski <lionheartcarpwiski@gmail.com>
2024-10-11 02:39:05 -0300

    Create BancoImobiliario.java

---
+++ BancoImobiliario.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+
+public class BancoImobiliario
+{
+   public static void main(String args[]) throws IOException
+   {
+      int num_jogadores;
+      Jogador[] vet_jogadores;
+      Tabuleiro tabuleiro = new Tabuleiro();
+   }
+}
+
+class Jogador
+{
+
+}
+
+class Tabuleiro
+{
+   Dado dado = new Dado();
+   Espaco[] vetor_espacos;
+}
+
+class Espaco
+{
+   
+}
+
+class Dado
+{
+   
+}
